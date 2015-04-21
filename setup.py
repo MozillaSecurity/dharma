47972cc94ed9be2936c465ec49e9c680649dc7df
Christoph Diehl <cdiehl@mozilla.com>
2015-04-21 22:57:08 +0200

    Make Dharma ready for PyPI

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
 	name = 'dharma',
 	packages = ['dharma'],
 	version = '1.0',
 	description = 'A generation-based, context-free grammar fuzzer.',
-	author = 'Mozilla Security Team',
+	author = 'Mozilla Security',
 	author_email = 'fuzzing@mozilla.com',
 	url = 'https://github.com/mozillasecurity/dharma',
-	download_url = 'https://github.com/mozillasecurity/dharma/archive/1.0.tar.gz',
+	download_url = 'https://github.com/mozillasecurity/dharma/tarball/1.0',
 	keywords = ['fuzzer', 'security', 'fuzzing', 'testing'],
-	classifiers = [],
+	classifiers = [ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+		'Intended Audience :: Developers',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+		'Topic :: Software Development :: Testing',
+	]
 )
-
-
