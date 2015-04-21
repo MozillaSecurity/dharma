6f4545a0bd55d218c5ea744d0ff4029a22310365
Christoph Diehl <cdiehl@mozilla.com>
2015-04-21 21:59:54 +0200

    Add necessary files for PyPI

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+	name = 'dharma',
+	packages = ['dharma'],
+	version = '1.0',
+	description = 'A generation-based, context-free grammar fuzzer.',
+	author = 'Mozilla Security Team',
+	author_email = 'fuzzing@mozilla.com',
+	url = 'https://github.com/mozillasecurity/dharma',
+	download_url = 'https://github.com/mozillasecurity/dharma/archive/1.0.tar.gz',
+	keywords = ['fuzzer', 'security', 'fuzzing', 'testing'],
+	classifiers = [],
+)
+
+
